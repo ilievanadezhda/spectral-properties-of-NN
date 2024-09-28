41fec60022c33488962e7cced3bc21677e23dd6a
Nadezhda Ilieva <nadezhda.ilieva@epfl.ch>
2024-09-28 19:03:03 +0200

    change experiments.ipynb

--- lanczos/utils.py
+++ lanczos/utils.py
@@ -17,3 +17,17 @@ def orthnormal(w, v_list):
     for v in v_list:
         w = w - torch.dot(w, v) * v
     return normalization(w)
+
+
+def generate_eigenvalues(size, dict_eigenvalues):
+    tensor = torch.zeros(size)
+    pos_extreme_indices = torch.randperm(size)[:dict_eigenvalues["num_pos_extremes"]]
+    neg_extreme_indices = torch.randperm(size)[:dict_eigenvalues["num_neg_extremes"]]
+    around_zero_indices = torch.randperm(size)[:dict_eigenvalues["num_around_zero"]] 
+    pos_extreme_values = torch.empty(dict_eigenvalues["num_pos_extremes"]).uniform_(dict_eigenvalues["pos_low"], dict_eigenvalues["pos_high"])
+    neg_extreme_values = torch.empty(dict_eigenvalues["num_neg_extremes"]).uniform_(dict_eigenvalues["neg_low"], dict_eigenvalues["neg_high"])
+    around_zero_values = torch.empty(dict_eigenvalues["num_around_zero"]).uniform_(dict_eigenvalues["around_zero_low"], dict_eigenvalues["around_zero_high"])
+    tensor[pos_extreme_indices] = pos_extreme_values
+    tensor[neg_extreme_indices] = neg_extreme_values
+    tensor[around_zero_indices] = around_zero_values
+    return tensor
